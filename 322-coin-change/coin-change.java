97bb9ed467633963a83f44f84fe7c8c024be5360
Saurabh Srivastava <163189524+SSrivastava18@users.noreply.github.com>
2026-06-16 12:21:16 +0530

    Time: 34 ms (14.09%) | Memory: 47.7 MB (7.84%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,35 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer[][] dp = new Integer[coins.length][amount + 1];
+
+        int[][] dp = new int[coins.length][amount + 1];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
         int ans = solve(0, amount, coins, dp);
+
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
-    
 
-    public int solve(int idx, int amount, int[] coins, Integer[][] dp) {
+    private int solve(int idx, int amount, int[] coins, int[][] dp) {
 
-        if(amount == 0) return 0;
-        if(idx == coins.length || amount < 0) return Integer.MAX_VALUE;
+        if (amount == 0) return 0;
 
-        if(dp[idx][amount] != null) return dp[idx][amount];
+        if (idx == coins.length || amount < 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (dp[idx][amount] != -1) {
+            return dp[idx][amount];
+        }
 
         int notTake = solve(idx + 1, amount, coins, dp);
 
         int take = Integer.MAX_VALUE;
         int res = solve(idx, amount - coins[idx], coins, dp);
-        if(res != Integer.MAX_VALUE){
+
+        if (res != Integer.MAX_VALUE) {
             take = 1 + res;
         }
 
