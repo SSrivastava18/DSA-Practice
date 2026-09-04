72376c1a53f4326a63d65da505c755c2963dca88
Saurabh Srivastava <163189524+SSrivastava18@users.noreply.github.com>
2026-09-04 16:22:36 +0530

    Time: 1 ms (87.88%) | Memory: 45.3 MB (78.59%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,28 +3,10 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
         helper(nums, new ArrayList<>(), used, ans);
-        // helper(nums, new ArrayList<>(), ans);
 
-        return ans;
-        
+        return ans;  
     }
-        // private void helper(int[] nums, List<Integer> curr, List<List<Integer>> ans) {
-        // if (curr.size() == nums.length) {
-        //     ans.add(new ArrayList<>(curr));
-        //     return;
-        // }
-
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (!curr.contains(nums[i])) {
-                
-        //         curr.add(nums[i]);
-
-        //         helper(nums, curr, ans);
-
-        //         curr.remove(curr.size() - 1);
-                
-        //     }
-        // }
+    
     private void helper(int[] nums, List<Integer> curr, boolean[] used, List<List<Integer>> ans) {
         if (curr.size() == nums.length) {
             ans.add(new ArrayList<>(curr));
@@ -33,6 +15,7 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (!used[i]) {
+
                 used[i] = true;
                 curr.add(nums[i]);
 
@@ -40,6 +23,7 @@ class Solution {
 
                 curr.remove(curr.size() - 1);
                 used[i] = false;
+                
             }
         }
     }
